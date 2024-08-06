6d4edfea332cd04606a796a53ef2852fb0cb0a05
janayaoshyn <janaya@oshyn.com>
2024-08-06 11:58:40 -0500

    Default hero variant

--- src/components/annual-report/Hero.tsx
+++ src/components/annual-report/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
   Field,
   Text,
@@ -6,6 +6,25 @@ import {
   ImageField,
   LinkField,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import Curve from 'assets/svg/Curve';
+import Pause from 'assets/svg/Pause';
+import useIsMobile from 'lib/customHooks/isMobile';
+import YouTube, { YouTubePlayer, YouTubeProps } from 'react-youtube';
+
+type MutePlayer = {
+  playerVars: {
+    mute: number;
+  };
+};
+
+const playerOpts: YouTubeProps['opts'] & MutePlayer = {
+  playerVars: {
+    autoplay: 1,
+    mute: 1,
+    controls: 0,
+    rel: 0,
+  },
+};
 
 interface Fields {
   Headline: Field<string>;
@@ -23,7 +42,58 @@ export type HeroProps = {
 export const Default = (props: HeroProps): JSX.Element => {
   const id = props.params.RenderingIdentifier;
 
+  const playerRef = useRef<YouTubePlayer | null>(null);
+  const [Playing, setPlaying] = useState(true);
+  const isMobile = useIsMobile(1024);
+
+  const onReady: YouTubeProps['onReady'] = (event) => {
+    playerRef.current = event.target;
+  };
+
+  const toggleVideo = () => {
+    if (!playerRef.current) return;
+    Playing ? playerRef.current.pauseVideo() : playerRef.current.playVideo();
+    setPlaying((v) => !v);
+  };
+
   console.log(props?.fields);
+  return (
+    <>
+      <div
+        className="hero relative"
+        style={{
+          backgroundImage: `url('${props.fields.Image.value?.src}')`,
+        }}
+      >
+        {!isMobile && (
+          <div className="hero__video-player">
+            <YouTube
+              className="w-full h-full absolute top-0 left-0 block"
+              videoId={props.fields.Video.value}
+              onReady={onReady}
+              opts={playerOpts}
+            />
+          </div>
+        )}
+        <div className="hero__background" />
+        <div className="hero__content text-center space-y-6 flex flex-col items-center">
+          <Text tag="h1" field={props.fields.Headline} />
+          <Text field={props.fields.Description} className="hidden lg:block" tag="p" />
+        </div>
+        {!isMobile && (
+          <button onClick={toggleVideo} className="p3 flex items-center gap-2.5">
+            <Pause /> {Playing ? 'Pause' : 'Play'} video
+          </button>
+        )}
+        <Curve />
+      </div>
+      <Text
+        field={props.fields.Description}
+        className="block lg:hidden font-medium container text-center py-10 pb-0 text-dark-blue"
+        tag="p"
+      />
+    </>
+  );
 
   return (
     <div
