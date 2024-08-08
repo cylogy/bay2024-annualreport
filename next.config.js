dbaf6a78e05881296fa3634327ec03eee9096e30
Prasanth Nittala <prasanth.nittala@courtavenue.com>
2024-08-07 19:04:16 -0700

    added redis_service_url and defined cache handler for standalone upper environment builds

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
   assetPrefix: publicUrl,
   assetPrefix: process.env.BUILD_STANDALONE === "true" ? publicUrl+"/"+process.env.ASSETS_BASE_FOLDER_PREFIX+"-"+process.env.CODEBUILD_BUILD_NUMBER : publicUrl,
 
-  cacheHandler: require.resolve('./cache-handler.js'),
+  cacheHandler: process.env.BUILD_STANDALONE === "true" ? require.resolve('./cache-handler.js'):undefined,
   // Allow specifying a distinct distDir when concurrently running app in a container
   distDir: process.env.NEXTJS_DIST_DIR || '.next',
 
