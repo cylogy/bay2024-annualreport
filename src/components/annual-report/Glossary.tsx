690869f2449e5f6facd510f6b6a95d01c99c7a64
Fernando Torres <ftorres@oshyn.com>
2024-08-13 17:17:03 -0500

    Glossary finished

--- src/components/annual-report/Glossary.tsx
+++ src/components/annual-report/Glossary.tsx
@@ -1,9 +1,14 @@
-import { useComponentProps, GetStaticComponentProps } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  useComponentProps,
+  GetStaticComponentProps,
+  RichText,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 import { getMainGlossary } from 'lib/graphql-utils';
 import { GLOSSARY_ITEM } from 'lib/constants';
 import { GlossaryQueryResult } from 'lib/graphql-queries/Glossary';
 import BlueSquareBullet from './atoms/BlueSquareBullet';
+import Accordion from './atoms/Accordion';
 
 type GlossaryProps = ComponentProps;
 
@@ -23,14 +28,18 @@ export const Default = (props: GlossaryProps): JSX.Element => {
       <section className="py-[60px] lg:py-[120px]">
         <div className="container grid gap-[96px]">
           {componentProps?.glossaryItems.mainGlossary.children.results.map((item, index) => (
-            <section key={index} className="flex gap-[70px]">
+            <section key={index} className="flex flex-col lg:flex-row gap-[20px] lg:gap-[70px]">
               <div>
                 <BlueSquareBullet text={item.name} />
               </div>
-              <div>
-                {item.children.results.map((term) => (
-                  <h1 key={term.name}>{term.term.jsonValue.value}</h1>
-                ))}
+              <div className="w-full lg:max-w-[835px]">
+                <Accordion>
+                  {item.children.results.map((term) => (
+                    <Accordion.Item Name={term.name} key={term.name}>
+                      <RichText field={term.description.jsonValue} className="richtext" />
+                    </Accordion.Item>
+                  ))}
+                </Accordion>
               </div>
             </section>
           ))}
