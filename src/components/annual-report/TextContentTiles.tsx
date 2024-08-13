4753442b73367e212b5c480d1fd58ab597098dc0
Fernando Torres <ftorres@oshyn.com>
2024-08-13 12:42:56 -0500

    Anchor Links first steps

--- src/components/annual-report/TextContentTiles.tsx
+++ src/components/annual-report/TextContentTiles.tsx
@@ -23,23 +23,56 @@ export const ThreeColumns = withDatasourceCheck()<TextContentTilesProps>(
   (props: TextContentTilesProps): JSX.Element => {
     return (
       /* Blue 3 cols */
-      <section className="py-[60px] lg:py-[120px] bg-dark-blue">
-        <div className="container lg:px-[70px]">
-          <Text
-            tag="h2"
-            className="text-center text-white mb-[10px]"
-            field={props.fields.Headline}
-          />
-          <RichText className="text-center text-white p1" field={props.fields.Description} />
-          <section className="grid lg:grid-cols-2 lg:grid-cols-3 gap-[40px] mt-[80px]">
-            <Placeholder
-              name={`text-content-tiles`}
-              rendering={props.rendering}
-              render={(components) => injectDynamicParams(components, { variant: 'ThreeColumns' })}
+      <>
+        {/* <section className="py-[60px] lg:py-[120px]">
+          <div className="container">
+            <div className="grid grid-cols-2 gap-[171px]">
+              <div></div>
+              <nav
+                id="anchorLinks"
+                className="bg-white rounded-[40px] p-[40px] text-dark-blue boxShadow"
+              >
+                <h4 className="text-dark-blue mb-[10px] border-b-[3px] border-solid pb-[20px]">
+                  On this page
+                </h4>
+                <ul className="mt-[30px]">
+                  <li>
+                    <a href="#">Overview</a>
+                  </li>
+                  <li>
+                    <a href="#">Reduce Health Impacts of Air Pollution</a>
+                  </li>
+                  <li>
+                    <a href="#">Hold Violators Accountable</a>
+                  </li>
+                  <li>
+                    <a href="#">Mitigate Climate Change and Its Impacts</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </section> */}
+        <section className="py-[60px] lg:py-[120px] bg-dark-blue">
+          <div className="container lg:px-[70px]">
+            <Text
+              tag="h2"
+              className="text-center text-white mb-[10px]"
+              field={props.fields.Headline}
             />
-          </section>
-        </div>
-      </section>
+            <RichText className="text-center text-white p1" field={props.fields.Description} />
+            <section className="grid lg:grid-cols-2 lg:grid-cols-3 gap-[40px] mt-[80px]">
+              <Placeholder
+                name={`text-content-tiles`}
+                rendering={props.rendering}
+                render={(components) =>
+                  injectDynamicParams(components, { variant: 'ThreeColumns' })
+                }
+              />
+            </section>
+          </div>
+        </section>
+      </>
     );
   }
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: {
       center: true,
       padding: {
-        DEFAULT: "15px",
+        DEFAULT: '15px',
       },
     },
     extend: {
       fontFamily: {
-        montserrat: ["var(--font-montserrat)"],
-        newsreader: ["var(--font-newsreader)"],
+        montserrat: ['var(--font-montserrat)'],
+        newsreader: ['var(--font-newsreader)'],
       },
       colors: {
-        "dark-blue": "var(--dark-blue)",
-        "light-green": "var(--light-green)",
-        "lighter-green": "var(--lighter-green)",
-        "powder-blue": "var(--powder-blue)",
-        mint: "var(--mint)",
-        sage: "var(--sage)",
-        ocean: "var(--ocean)",
-        fern: "var(--fern)",
-        white: "var(--white)",
-        "soft-white": "var(--soft-white)",
+        'dark-blue': 'var(--dark-blue)',
+        'light-green-10': 'var(--light-green-10)',
+        'light-green': 'var(--light-green)',
+        'lighter-green': 'var(--lighter-green)',
+        'powder-blue': 'var(--powder-blue)',
+        mint: 'var(--mint)',
+        sage: 'var(--sage)',
+        ocean: 'var(--ocean)',
+        fern: 'var(--fern)',
+        white: 'var(--white)',
+        'soft-white': 'var(--soft-white)',
       },
       screens: {
-        xs: "480px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
       },
     },
   },
   plugins: [],
-}
-
+};
