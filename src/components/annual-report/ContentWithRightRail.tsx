7ad3d7e2cdcae4017bab1e40820447441db95e25
Mayra Olivo <molivo@oshyn.com>
2024-08-15 08:40:26 -0700

    Content With Right Rail

---
+++ src/components/annual-report/ContentWithRightRail.tsx
@@ -0,0 +1,30 @@
+import { LayoutServiceData, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type ContentWithRightRailProps = ComponentProps & {
+  layoutData: LayoutServiceData;
+};
+
+export const Default = (props: ContentWithRightRailProps): JSX.Element => {
+  return (
+    <>
+      <div className="col-span-full md:col-span-9">
+        Left Content
+        <Placeholder
+          name="left-content"
+          rendering={props.rendering}
+          layoutData={props.layoutData}
+        />
+      </div>
+
+      <div className="col-span-full md:col-span-3">
+        Right Content
+        <Placeholder
+          name="right-content"
+          rendering={props.rendering}
+          layoutData={props.layoutData}
+        />
+      </div>
+    </>
+  );
+};
