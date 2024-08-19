65a6106063444dbce81f5ca05987d51d6366599a
Mayra Olivo <molivo@oshyn.com>
2024-08-19 14:51:38 -0500

    Breadcrumb check

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -55,13 +55,25 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
       {/* root placeholder for the app, which we add components to using route data */}
       <div className={mainClassPageEditing}>
         <header>
-          <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
+          <div id="header">
+            {route && (
+              <Placeholder name="headless-header" rendering={route} layoutData={layoutData} />
+            )}
+          </div>
         </header>
         <main>
-          <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
+          <div id="content">
+            {route && (
+              <Placeholder name="headless-main" rendering={route} layoutData={layoutData} />
+            )}
+          </div>
         </main>
         <footer>
-          <div id="footer">{route && <Placeholder name="headless-footer" rendering={route} />}</div>
+          <div id="footer">
+            {route && (
+              <Placeholder name="headless-footer" rendering={route} layoutData={layoutData} />
+            )}
+          </div>
         </footer>
         <ScrollToTop />
       </div>
