c39eb9b2e120a7a7943a3fab1a539a21b36eaf5b
janayaoshyn <janaya@oshyn.com>
2024-08-27 14:03:03 -0500

    Captcha validation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -18,6 +19,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="beforeInteractive"
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}`}
+        />
       </body>
     </Html>
   );
