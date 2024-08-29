246bfa36bd7aa41e6e1bea27acfaae2d388d8aaa
Prasanth Nittala <prasanth.nittala@courtavenue.com>
2024-08-29 16:59:35 -0700

    process.env.vercel check

--- src/lib/editing/editing.ts
+++ src/lib/editing/editing.ts
@@ -1,18 +1,16 @@
-//import { BasicEditingDataService } from './editing-data-service';
 import {
   BasicEditingDataService,
   ServerlessEditingDataService,
 } from '@sitecore-jss/sitecore-jss-nextjs/editing';
-//import { redisEditingDataCache } from './redis-editing-data-cache';
-import { netlifyEditingDataCache } from './netlify-editing-data-cache';
+import { redisEditingDataCache } from './redis-editing-data-cache';
 
 // Override the default editingDataDiskCache with an accessible temp directory
-export const myEditingDataCache = netlifyEditingDataCache;
+export const myEditingDataCache = redisEditingDataCache;
 //export const myEditingDataCache = redisEditingDataCache;
 
 // Override default editingDataService to use myEditingDataDiskCache for BasicEditingDataService
 export const myEditingDataService =
-  process.env.VERCEL === 'true'
+  process.env.VERCEL
     ? new ServerlessEditingDataService()
     : new BasicEditingDataService({
         editingDataCache: myEditingDataCache,
