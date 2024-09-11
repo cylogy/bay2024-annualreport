6514e93221f49c2535cbc8febebbd850e38103b9
janayaoshyn <janaya@oshyn.com>
2024-09-11 12:16:04 -0500

    Styles, nextimage, header, hero video, performance

--- src/components/annual-report/atoms/NextImage.tsx
+++ src/components/annual-report/atoms/NextImage.tsx
@@ -35,7 +35,7 @@ export default function NextImage({ field, ...props }: NextImageProps) {
       src={src ?? ''}
       alt={value?.alt ?? ''}
       fill={!hasDimensions}
-      sizes="(min-width: 768px) 100vw, (max-width: 768px) 50vw, 33vw"
+      sizes="(min-width: 768px) 100vw, 70vw"
       height={hasDimensions ? Number(value?.height) : undefined}
       width={hasDimensions ? Number(value?.width) : undefined}
       quality={90}
