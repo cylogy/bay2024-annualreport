315eab1e6d61d18a29dba674da610905ce622e54
Prasanth Nittala <prasanth.nittala@courtavenue.com>
2024-09-11 13:25:51 -0700

    setting cache control headers

--- src/lib/middleware/plugins/basic-auth-middleware.ts
+++ src/lib/middleware/plugins/basic-auth-middleware.ts
@@ -11,7 +11,7 @@ class BasicAuthMiddlewarePlugin implements MiddlewarePlugin {
    * @param req<NextRequest>
    * @returns Promise<NextResponse>
    */
-  async exec(req: NextRequest): Promise<NextResponse> {
+  async exec(req: NextRequest, res?: NextResponse): Promise<NextResponse> {
     const url = req.nextUrl;
     debug.common('basicauth middlewareurl:' + url.pathname);
     debug.common('basicauth:' + process.env.ENABLE_BASIC_AUTH);
@@ -42,6 +42,8 @@ class BasicAuthMiddlewarePlugin implements MiddlewarePlugin {
 
         if (user === process.env.BASIC_AUTH_USERNAME && pwd === process.env.BASIC_AUTH_PASSWORD) {
           debug.common('passed authentication test');
+          debug.common('current response cache control:' + res?.headers.get('cache-control'));
+          res?.headers.set('cache-control', 'no-store');
           return NextResponse.next();
         }
       }
